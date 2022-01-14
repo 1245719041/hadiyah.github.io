0110d0ce268f4a42a5a6fbdf93f49abb3baedc7f
1245719041 <97719562+1245719041@users.noreply.github.com>
2022-01-14 14:08:39 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+	<title>Siti Hadiyah</title>
+	<!-- menghubungkan dengan file css -->
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<!-- menghubungkan dengan file jquery -->
+	<script type="text/javascript" src="jquery.js"></script>
+</head>
+<body>
+<!-- 
+Author : diki alfarabi hadi 
+Site : www.malasngoding.com
+-->
+<div class="content">
+	<header>
+		<h1 class="judul">TUGAS UAS</h1>
+		<h3 class="deskripsi">Siti Hadiyah</h3>
+	</header>
+ 
+	
+ 
+	</div>
+</div>
+</body>
+</html>
